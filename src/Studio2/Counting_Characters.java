7fc896d323429c948e5c750c4dfc259c4645fba6
Eydie <you@example.com>
2021-07-21 20:36:36 -0500

    submitting studio 2 for java but still will work on the last part of the bonus

---
+++ src/Studio2/Counting_Characters.java
@@ -0,0 +1,34 @@
+package Studio2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Counting_Characters {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a phrase:");
+        String inputStr = input.nextLine();
+        char[] charInString = inputStr.toCharArray();
+
+        String allowableChar = "a-z";
+
+        HashMap<Character, Integer> charCounts = new HashMap<>();
+
+        for (char letter: charInString) {
+            if (charCounts.containsKey(letter)) {
+                charCounts.put(letter, charCounts.get(letter) + 1);
+            } else {
+                charCounts.put(letter, 1);
+            }
+        }
+        for {char alphabet: allowableChar) {
+        if (charInString != allowableChar) {
+
+        }
+        }
+        for (Map.Entry<Character, Integer> printChar : charCounts.entrySet()) {
+            System.out.println(printChar.getKey() + ":" + printChar.getValue());
+        }
+    }
+}
