c6b179aa24060c4483bf74757acbd3c6b9f74412
Eydie <you@example.com>
2021-07-26 19:21:31 -0500

    Commit Exercises for Java week 2

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -3,11 +3,45 @@ package org.launchcode.java.demos.lsn3classes1;
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
-public class Student {
+public class  Student {
 
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+
+    public Student(String name,int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    private String getName() {
+        return name;
+    }
+    private void setName(String name) {
+        this.name = name;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public void setNumberOfCredits(int CreditNumber) {
+        this.numberOfCredits = CreditNumber;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+
 
 }
