09191335ec0a32e7e1998f166c538436495ea424
Eydie <you@example.com>
2021-08-09 12:05:23 -0500

    test push for Ted to see

--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -1,12 +1,41 @@
+
 package org.launchcode.java.demos.lsn4classes2;
 
 import java.util.ArrayList;
 
-public class Course {
-    private String topic;
-    private Teacher instructor;
-    private ArrayList<Student> enrolledStudents;
+    public class Course {
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
 
+        // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
+        //  just the class fields.
+        public String toString() {
+            return (this.topic + " will be taught by " + Teacher.instructor.firstName);
+        }
+
+
+        // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
+        //  Course objects equal.
+
+        public boolean equals(Object toBeCompared) {
+            if (toBeCompared == this) {
+                return true;
+            }
+
+            if (toBeCompared == null) {
+                return false;
+            }
+
+            if (toBeCompared.getClass() != getClass()) {
+                return false;
+            }
+
+
+            return theCourse.topic == theCourse.topic;
+        }
+
+    }
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
 
